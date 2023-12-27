4895171b98f0197addc3eb84cc64168bb23cf6ca
Kateryna BRYLINSKA <kateryna.brylinska@legrand.com>
2023-12-27 15:32:26 +0100

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kateryna BRYLINSKA on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
