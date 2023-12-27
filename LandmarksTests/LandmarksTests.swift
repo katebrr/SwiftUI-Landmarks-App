9dfcd6b28714131417b4aed29b3afb00a620a97c
Kateryna BRYLINSKA <kateryna.brylinska@legrand.com>
2023-12-27 15:51:52 +0100

    initial commit

--- LandmarksTests/LandmarksTests.swift
+++ LandmarksTests/LandmarksTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Landmarks
 
 final class LandmarksTests: XCTestCase {
 
@@ -27,7 +28,7 @@ final class LandmarksTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
